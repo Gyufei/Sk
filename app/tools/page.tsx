1bb0a11f5b1cccf851d380af731606278cfc63dd
william <william.s@hedgue.com>
2024-05-09 04:11:44 -0700

    feat:remove dataurl regex test

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -15,13 +15,14 @@ export default function Tools() {
   const { submitDataUrl } = useSubmitDataUrl();
 
   async function handleSubmit() {
-    setStatusText("Sending...");
+    if (statusText == "sending...") {
+      return;
+    }
+    setStatusText("sending...");
 
     const regex =
-      /^https:\/\/(twitter|x).com\/(?<twitter_id>[a-zA-Z0-9_-]{2,15})\/status\/(?<tweet_id>\d{10,20})/g;
+      /https:\/\/(twitter|x).com\/(?<twitter_id>[a-zA-Z0-9_-]{2,15})\/status\/(?<tweet_id>\d{10,20})/g;
 
-    if (!regex.test(dataUrl)) return;
-    regex.lastIndex = 0;
     const regResult = regex.exec(dataUrl);
 
     if (!regResult || !regResult.groups) return;
