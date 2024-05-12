704184d13e07f9fdd2b79b0043bfb5a44ea28579
fei guo <812773673@qq.com>
2024-05-12 22:28:43 +0800

    fix:uuid-redirect

--- lib/use-fetch-user-info.ts
+++ lib/use-fetch-user-info.ts
@@ -1,25 +1,34 @@
 import { useAtom, useSetAtom } from "jotai/react";
 import { UserInfoAtom, UuidAtom } from "./state";
-import { useEffect } from "react";
 import { ApiHost } from "./path";
 import useSWR from "swr";
 import fetcher from "./fetcher";
+import { useRouter } from "next/navigation";
 
 export function useFetchUserInfo() {
   const [uuid, setUuid] = useAtom(UuidAtom);
   const setUserInfo = useSetAtom(UserInfoAtom);
+  const router = useRouter();
 
-  const res = useSWR(uuid ? `${ApiHost}/user/info?user_id=${uuid}` : null, fetcher);
+  async function getUserInfo() {
+    if (!uuid) return;
 
-  useEffect(() => {
+    const res: any = await fetcher(`${ApiHost}/user/info?user_id=${uuid}`);
     if (res.data) {
       setUserInfo(res.data);
-    } else {
-      console.log('no data', res.data);
+      return;
+    } 
+
+    if (uuid && res.data === false) {
       setUserInfo({});
       setUuid("");
+      router.push('/club');
     }
-  }, [res.data, setUserInfo, setUuid]);
+
+    return res;
+  }
+
+  const res = useSWR(uuid, getUserInfo);
 
   return {
     ...res,
