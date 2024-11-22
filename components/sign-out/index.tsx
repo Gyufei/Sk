e87b05399965c9f7d101d9f4697a5b3577739748
fei guo <812773673@qq.com>
2024-11-22 19:07:14 +0800

    style:some-import

--- components/sign-out/index.tsx
+++ components/sign-out/index.tsx
@@ -1,25 +1,24 @@
 import { useTranslations } from "next-intl";
-import { useAtom } from "jotai/react";
+import { useSetAtom } from "jotai/react";
 import { UuidAtom } from "@/lib/api/state";
 
 function SignOut() {
   const T = useTranslations("Common");
-  const [uuid, setUuid] = useAtom(UuidAtom);
+  const setUuid = useSetAtom(UuidAtom);
 
   function handleSignOut() {
     setUuid("");
-    // 刷新当前页面
-    location.reload()
+    location.reload();
   }
 
   return (
     <div
       onClick={handleSignOut}
-      className="normal-line-button mt-[80px] flex w-[137px] h-12 cursor-pointer items-center justify-center rounded-lg border whitespace-nowrap px-[100px] text-base leading-6 hover:bg-red-500 hover:border-none hover:text-white"
+      className="normal-line-button mt-[80px] flex h-12 w-[137px] cursor-pointer items-center justify-center whitespace-nowrap rounded-lg border px-[100px] text-base leading-6 hover:border-none hover:bg-red-500 hover:text-white"
     >
       {T("SignOut")}
     </div>
-  )
+  );
 }
 
 export default SignOut;
