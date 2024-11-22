e3f22bdd2bd297818a25fce59edc589179f938f4
beewolf <biw725704@gmail.com>
2024-11-22 14:17:33 +0800

    fix: card icon style

--- app/[local]/mart/page.tsx
+++ app/[local]/mart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { formatNum } from "@/lib/utils/number";
 import { IProduct, useMartProducts } from "@/lib/api/use-mart-products";
 import { useMartBuy } from "@/lib/api/use-mart-buy";
@@ -11,7 +11,6 @@ import MartMenu from "./mart-menu";
 export default function MartPage() {
   const T = useTranslations("Common");
   const { setGlobalMessage } = useContext(GlobalMsgContext);
-  const [hoverIndex, setHoverIndex] = useState(-1);
 
   const { data: products } = useMartProducts();
 
@@ -38,32 +37,27 @@ export default function MartPage() {
     }
   }, [isMutating, T, setGlobalMessage]);
 
-  const productClass = 'flex-1 sm:flex-auto sm:w-[240px] md:w-[250px]'
+  const productClass = 'flex-1 sm:flex-auto sm:w-[240px] md:w-[250px]';
 
   return (
     <div className="relative h-full">
       <div className="z-50 fixed inset-x-4 bottom-8 sm:absolute sm:left-[auto] sm:right-[-120px] sm:top-[5px] mt-6"><MartMenu /></div>
       <div className="pd-[100px] gap-x-[10px] sm:pd-0 sm:trans-scroll-bar content-w-540 mt-6 md:-ml-[250px] flex h-fit md:max-h-[calc(100%-70px)] flex-wrap sm:gap-x-[10px] gap-y-5 md:overflow-y-auto align-top md:pr-3">
-        {(products || []).map((item, index) => (
+        {(products || []).map((item) => (
           <div
             key={item.product_id}
-            className={`${productClass} box-border flex h-fit cursor-pointer justify-center rounded-[20px] border border-transparent hover:border-white md:p-[5px]`}
-            onMouseEnter={() => setHoverIndex(index)}
-            onMouseLeave={() => setHoverIndex(-1)}
+            className={`${productClass} group box-border flex h-fit cursor-pointer justify-center rounded-[20px] border border-transparent hover:border-white md:p-[5px]`}
           >
-            <div className="h-[265px] sm:w-[240px] flex sm:h-[350px] flex-col rounded-[20px] bg-[rgba(255,255,255,0.1)] backdrop-blur-md">
+            <div className="min-w-[165px] w-full sm:w-[240px] flex sm:h-[350px] flex-col rounded-[20px] bg-[rgba(255,255,255,0.1)] backdrop-blur-md">
               <Image
                 src={item.product_display_picture || "/images/590.png"}
                 width={240}
                 height={240}
                 alt="mart"
-                className="content-w-165 content-h-165 rounded-[20px] "
+                className="w-full rounded-[20px] "
               ></Image>
               <div
-                className="p-[15px]"
-                style={{
-                  color: hoverIndex === index ? "white" : "#d6d6d6",
-                }}
+                className="p-[15px] #d6d6d6 group-hover:text-white"
               >
                 <div className="break-words text-base font-medium leading-6">
                   {item.product_name}
@@ -76,25 +70,22 @@ export default function MartPage() {
                     </span>
                   </div>
                   <div
-                    className="flex h-8 w-8 items-center justify-center rounded-full"
-                    style={{
-                      backgroundColor:
-                        hoverIndex === index
-                          ? "rgba(255,255,255,0.1)"
-                          : "rgba(214, 214, 214, 0.1)",
-                    }}
+                    onClick={() => handleBuy(item)}
+                    className="flex h-8 w-8 items-center justify-center rounded-full bg-[rgba(214,214,214,0.1)] group-hover:bg-[rgba(255,255,255,0.1)]"
                   >
                     <Image
-                      onClick={() => handleBuy(item)}
-                      src={
-                        hoverIndex === index
-                          ? "/icons/buy-car.svg"
-                          : "/icons/buy-car-gray.svg"
-                      }
                       width={18}
                       height={18}
-                      alt="buy-car"
-                      className="cursor-pointer"
+                      alt={"buy-car"}
+                      className={"group-hover:hidden"}
+                      src={"/icons/buy-car-gray.svg"}
+                    />
+                    <Image
+                       width={18}
+                       height={18}
+                       alt={"buy-car"}
+                      className={"hidden group-hover:inline-block"}
+                      src={"/icons/buy-car.svg"}
                     />
                   </div>
                 </div>
