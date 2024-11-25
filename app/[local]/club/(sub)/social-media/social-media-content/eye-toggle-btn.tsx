b728613d044183c6c6ea36eaab5a63bed380f942
beewolf <biw725704@gmail.com>
2024-11-25 13:56:48 +0800

    fix: fix conflicts

--- app/[local]/club/(sub)/social-media/social-media-content/eye-toggle-btn.tsx
+++ app/[local]/club/(sub)/social-media/social-media-content/eye-toggle-btn.tsx
@@ -1,62 +1,50 @@
 import { IconBtn } from "@/components/icon-btn";
-import {
-  Tooltip,
-  TooltipProvider,
-  TooltipTrigger,
-  TooltipContent,
-} from "@/components/ui/tooltip";
+import { WithTip } from "@/components/with-tip";
 import { useState } from "react";
 
-export function useEyeToggle({ keyword }: { keyword: string }): {
+export function useEyeToggle({
+  keyword
+}: {
+  keyword: string
+}): {
   eyeState: boolean;
   handleToggle: (newEyeState: boolean) => void;
 } {
-  const eyeDefaultState = !!localStorage.getItem(keyword);
+  const  eyeDefaultState = !!localStorage.getItem(keyword);
   const [eyeState, setEyeState] = useState<boolean>(eyeDefaultState);
   const handleToggle = (newEyeState: boolean) => {
     setEyeState(newEyeState);
     if (newEyeState) {
-      localStorage.setItem(keyword, "true");
+      localStorage.setItem(keyword, 'true')
     } else {
-      localStorage.removeItem(keyword);
+      localStorage.removeItem(keyword)
     }
-  };
+  }
   return {
     eyeState,
-    handleToggle,
+    handleToggle
   };
-}
+} 
 export function EyeToggleBtn({
   eyeState,
-  handleToggle,
+  handleToggle
 }: {
   eyeState: boolean;
   handleToggle: (newEyeState: boolean) => void;
 }) {
+  
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <IconBtn
-            mobileHoverColorChanged={false}
-            className={
-              "absolute right-[-5px] top-[-10px] mt-0 border-0 md:static md:mt-[0px] md:border"
-            }
-            defaulImage={
-              eyeState === true ? "/icons/eye-open.svg" : "/icons/eye-close.svg"
-            }
-            hoverImage={
-              eyeState === true
-                ? "/icons/eye-open-black.svg"
-                : "/icons/eye-close-black.svg"
-            }
-            handleClick={() => handleToggle(!eyeState)}
-          />
-        </TooltipTrigger>
-        <TooltipContent>
-          <div>{eyeState === true ? "Display account" : "Hide account"}</div>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  );
+    <WithTip 
+      className={'min-w-[154px]'} 
+      tipContent={<p>{eyeState === true ? 'Display account' : 'Hide account'}</p>}>
+       <IconBtn
+          mobileHoverColorChanged={false}
+          className={'border-0 md:border absolute mt-0 right-[-5px] top-[-10px] md:static md:mt-[0px]'}
+          defaulImage = {eyeState === true ? "/icons/eye-open.svg" : "/icons/eye-close.svg"}
+          hoverImage = {eyeState === true ? "/icons/eye-open-black.svg" : "/icons/eye-close-black.svg"}
+          handleClick={() => handleToggle(!eyeState)}
+        />
+    </WithTip>
+  )
 }
+
