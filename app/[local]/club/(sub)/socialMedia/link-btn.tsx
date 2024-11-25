b818168dffcd81e4d9a5c107040bf20955590d1b
beewolf <biw725704@gmail.com>
2024-11-25 13:49:48 +0800

    fix: media button disabled

--- app/[local]/club/(sub)/socialMedia/link-btn.tsx
+++ app/[local]/club/(sub)/socialMedia/link-btn.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { useState } from "react";
 
 export function LinkBtn({
   onClick,
@@ -10,7 +9,6 @@ export function LinkBtn({
   disabled: boolean;
   isConnected: boolean;
 }) {
-  const [isHover, setIsHover] = useState(false);
 
   const handleClick = () => {
     if (!disabled) {
@@ -18,24 +16,30 @@ export function LinkBtn({
     }
   };
 
-  const showSrc = isConnected
-    ? isHover
-      ? "/icons/unlink-white.svg"
-      : "/icons/linked.svg"
-    : isHover
-    ? "/icons/link-black.svg"
-    : "/icons/link.svg";
-
+  const connectedClassHover = "data-[connected=true]:data-[disabled=false]:hover:border-[#FF5A5A] data-[connected=true]:data-[disabled=false]:hover:bg-[#FF5A5A]";
+  const notConnectedClassHover = "data-[notconnected=true]:data-[disabled=false]:hover:border-[#FFF] data-[notconnected=true]:data-[disabled=false]:hover:bg-[#FFF]";
   return (
     <div
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
       onClick={handleClick}
-      data-disabled={disabled}
-      data-connected={isConnected}
-      className="ml-0 mt-4 flex h-12 w-full cursor-pointer items-center justify-center rounded-lg border border-[rgba(255,255,255,0.6)] data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50 data-[connected=true]:hover:border-[#FF5A5A] data-[connected=true]:hover:bg-[#FF5A5A] data-[disabled=false]:hover:bg-[#FF5A5A] md:ml-4 md:mt-0 md:w-12"
+      data-disabled={disabled ? true : false}
+      data-connected={isConnected ? true : false}
+      data-notconnected={!isConnected}
+      className={`group ${disabled ? 'is-disabled' : 'isnot-disabled'} ml-0 mt-4 flex h-12 w-full cursor-pointer items-center justify-center rounded-lg border border-[rgba(255,255,255,0.6)] data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50 ${connectedClassHover} ${notConnectedClassHover} md:ml-4 md:mt-0 md:w-12`}
     >
-      <Image src={showSrc} width={24} height={24} alt="save" />
+      <Image
+        className={'inline-block md:group-[.isnot-disabled]:group-hover:hidden'}
+        src={isConnected ? "/icons/linked.svg" : "/icons/link.svg"}
+        width={24}
+        height={24}
+        alt="save"
+      />
+      <Image
+        className={'hidden md:group-[.isnot-disabled]:group-hover:inline-block'}
+        src={isConnected ? "/icons/unlink-white.svg" : "/icons/link-black.svg"}
+        width={24}
+        height={24}
+        alt="save"
+      />
       <div className="ml-1 text-base leading-6 md:hidden">
         {isConnected ? "Disconnect" : "Connect"}
       </div>
