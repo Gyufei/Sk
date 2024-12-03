e091549b1a595e02fcf09ee9e2cf149223be45cc
beewolf <biw725704@gmail.com>
2024-12-03 12:08:48 +0800

    fix: rainbow locale

--- components/provider/wallet-context/index.tsx
+++ components/provider/wallet-context/index.tsx
@@ -6,7 +6,6 @@ import { arbitrum, base, bsc, linea, mainnet, optimism, polygon, ronin, sepolia,
 import { darkTheme, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 
-
 const chains = [
   mainnet,
   optimism,
@@ -38,11 +37,18 @@ const chains = [
 
 const queryClient = new QueryClient()
 
+function localeCheck() {
+  const url = window.location.href;
+  if (url.includes('/en')) return "en";
+  return "zh-CN"
+}
+
 export function Web3Provider({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider
+          locale={localeCheck()}
           theme={darkTheme({
             accentColor: '#7b3fe4',
             accentColorForeground: 'white',
