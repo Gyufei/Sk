182990a7d4dd27ae0e34bc2b4bc2bfe50c365c61
beewolf <biw725704@gmail.com>
2024-12-24 20:09:48 +0800

    feat: change event data post way

--- lib/api/use-claim-tokens.ts
+++ lib/api/use-claim-tokens.ts
@@ -27,23 +27,9 @@ function chunkArray<T>(array: T[], size: number): T[][] {
 
 export function useClaimTokens() {
   async function fetchAllEventsData() {
-    const res: Array<{
-      project_name: string;
-      claim_version: number;
-    }> = await fetcher(`${ApiHost}/static/events.json`);
-
-    const eventData = await Promise.all(
-      res.map(async (item) => {
-        const eD = await fetcher(
-          `${ApiHost}/events?project_name=${item.project_name}`,
-        );
-        return {
-          ...item,
-          ...eD,
-        };
-      }),
+    const eventData = await fetcher(
+      `${ApiHost}/events`,
     );
-
     return eventData;
   }
 
