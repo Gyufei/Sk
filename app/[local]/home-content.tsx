d3c6bd485fb56269d4196d5a31eaf6ca4a033721
beewolf <biw725704@gmail.com>
2024-12-25 14:44:55 +0800

    feat: change login way  homepage can see

--- app/[local]/home-content.tsx
+++ app/[local]/home-content.tsx
@@ -2,14 +2,15 @@
 import { useAtomValue } from "jotai/react";
 import { UuidAtom } from "@/lib/api/state";
 import SignDialog from "@/components/sign-dialog";
+import { usePathname } from "@/app/navigation";
 
 export default function HomeContent({ children }: { children: React.ReactNode }) {
   const uuid = useAtomValue(UuidAtom);
-
+  const pathname = usePathname();
 
   return (
     <div className="right-block content-container md:min-h-unset relative min-h-[200.0025px]">
-      {uuid ? children : <SignDialog />}
+      {uuid || pathname === '/home' ? children : <SignDialog />}
     </div>
   );
 }
