28032806d1c367b07aca7948f081fe8f75014791
william <william.s@hedgue.com>
2025-01-02 02:07:23 -0800

    fix: Hide temporarily asset mint

--- app/[local]/club/(sub)/assets/page.tsx
+++ app/[local]/club/(sub)/assets/page.tsx
@@ -7,18 +7,18 @@ const mockList = ["A", "B", "C", "D"];
 
 export default function Page() {
   return (
-    <div className="m-t-20 relative content-w-760">
+    <div className="m-t-20 content-w-760 relative">
       <div className="relative flex flex-row-reverse items-end justify-between sm:flex-row">
         <BreadCrumbs />
         <GoBackTo />
       </div>
       <div className="mb-[20px] mt-6">
         <div className="flex flex-wrap justify-between gap-5 sm:flex-nowrap">
-          <div className="flex w-full flex-wrap items-center justify-between gap-[15px] sm:gap-[20px] sm:w-[520px]">
+          <div className="flex w-full flex-wrap items-center justify-between gap-[15px] sm:w-[520px] sm:gap-[20px]">
             {mockList.map((item, index) => {
               return (
                 <div
-                  className="flex w-[44vw] h-[44vw] sm:h-[240px] sm:w-[240px] flex-col items-center justify-center rounded-[20px] bg-[rgba(255,255,255,0.1)] backdrop-blur-md "
+                  className="flex h-[44vw] w-[44vw] flex-col items-center justify-center rounded-[20px] bg-[rgba(255,255,255,0.1)] backdrop-blur-md sm:h-[240px] sm:w-[240px] "
                   key={index}
                 ></div>
               );
@@ -34,23 +34,23 @@ export default function Page() {
                 alt="mint wallet"
               />
             </div>
-            <div className="font-haasDisp">
+            {/* <div className="font-haasDisp">
               <div className="flex h-12 cursor-pointer items-center justify-center rounded-lg border border-[rgba(255,255,255,0.6)] bg-[rgba(255,255,255,0.01)] px-4 text-base leading-6 text-[rgba(255,255,255,0.6)] hover:text-white md:mt-5 md:w-[200px]">
                 <span className="inline-block font-semibold md:mr-6">Mint</span>
                 <span>on</span>
                 <Image
-                  src="/icons/network/solana.svg"
+                  src="/icons/network/ethereum.svg"
                   width={16}
                   height={16}
-                  alt="solana"
+                  alt="ethereum"
                   className="ml-2 mr-1"
                 />
-                <span>Solana</span>
+                <span>Ethereum</span>
               </div>
               <div className="mt-3 text-center text-base opacity-80">
-                Eligible item: 3
+                Eligible item: 0
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
