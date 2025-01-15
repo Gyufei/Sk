60040198963db646a36e1daf686df98bfb43f1c2
beewolf <biw725704@gmail.com>
2025-01-15 19:38:45 +0800

    fix: spell wrong

--- lib/api/path.ts
+++ lib/api/path.ts
@@ -3,7 +3,7 @@ export const isProduction = process.env.NODE_ENV === "production" && !isPreview;
 
 const ProdHost = "https://api.juu17.com";
 const DevHost = "https://juu17-api-dev.vercel.app";
-const ProdSocket = "wss://notion.juu17.comm";
+const ProdSocket = "wss://notion.juu17.com";
 const DevSocket = "wss://preview-notion.juu17.com";
 
 export const ApiHost = isProduction ? ProdHost : DevHost;
