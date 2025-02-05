c611fc26f8173f234ba160ec40d14c8ffdd60ca9
william <william.s@hedgue.com>
2025-02-05 08:49:01 -0800

    upgrade: change RPC endpoints

--- components/solana-provider.tsx
+++ components/solana-provider.tsx
@@ -13,9 +13,10 @@ import { clusterApiUrl } from "@solana/web3.js";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 const MainnetRpc =
-  "https://ultra-icy-valley.solana-mainnet.quiknode.pro/8e93a1a6537b875fef5f9a4f6f0499aec020bfa8";
+  "https://mainnet.helius-rpc.com/?api-key=6e25109f-21ec-4fd2-9e2d-027213f5cd6e";
 const DevnetRpc =
-  "https://rpc.ankr.com/solana_devnet" || clusterApiUrl("devnet");
+  "https://devnet.helius-rpc.com/?api-key=6e25109f-21ec-4fd2-9e2d-027213f5cd6e" ||
+  clusterApiUrl("devnet");
 
 export function SolanaWalletProviders({ children }: { children?: ReactNode }) {
   const rpc = process.env.NODE_ENV === "production" ? MainnetRpc : DevnetRpc;
