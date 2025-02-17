9563c9d3e1616d563a7ce444efc1b20a3d3a95a0
fei guo <812773673@qq.com>
2025-02-17 18:27:34 +0800

    feat:callback-all-to-login

--- lib/use-sign-callback-url.ts
+++ lib/use-sign-callback-url.ts
@@ -1,12 +1,6 @@
-import { useSearchParams } from "next/navigation";
-
 export function useSignCallbackUrl() {
-  const searchParams = useSearchParams();
-  const from = searchParams.get("from");
-
   function getCallbackUrl() {
-    const path = from ? `/${from}` : "/home";
-    return window.location.origin + path;
+    return window.location.href;
   }
 
   return { getCallbackUrl };
