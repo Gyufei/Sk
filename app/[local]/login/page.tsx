51b507dc59939df358e2133f67de581f656b2805
fei guo <812773673@qq.com>
2025-02-17 19:19:33 +0800

    fix:mb-style

--- app/[local]/login/page.tsx
+++ app/[local]/login/page.tsx
@@ -138,7 +138,7 @@ export default function SignDialog() {
 
   return (
     <div
-      className={`ml-[-250px] flex w-[345px] font-haasDisp sm:w-[400px] ${
+      className={`sm:ml-[-250px] flex w-[345px] font-haasDisp sm:w-[400px] ${
         noMethodShow
           ? "mt-[100px] justify-center bg-transparent px-0 py-[40px] sm:mt-0 sm:h-[500px] sm:w-[500px] sm:bg-transparent"
           : "bg-[rgba(255,255,255,0.1)] p-[35px]"
