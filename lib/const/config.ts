8638854470ef0a0ad6863c446b296427664bb2b4
fei guo <812773673@qq.com>
2025-03-10 20:07:04 +0800

    fix:env-config

--- lib/const/config.ts
+++ lib/const/config.ts
@@ -1,15 +1,15 @@
 import { isProduction } from "../api/path";
 
 export const GoogleClientId = isProduction
-  ? ""
+  ? "369418276123-o08grpmn84s7bboj25u7mp9r1p8903ne.apps.googleusercontent.com"
   : "901355214678-litp58raporctginmttjk7jaqa0t7dsb.apps.googleusercontent.com";
 
 export const TgConfig = isProduction
   ? {
-      botName: "",
-      botId: "",
-    }
-  : {
       botName: "Juu17SiteBot",
       botId: "7905537340",
+    }
+  : {
+      botName: "Juu17SiteDevBot",
+      botId: "7941117201",
     };
