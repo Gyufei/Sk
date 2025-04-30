9ec46bda92697666793aa0526b21b742934d772d
fei guo <812773673@qq.com>
2025-04-30 12:38:27 +0800

    fix:error-left-top-link

--- app/[local]/home-logo.tsx
+++ app/[local]/home-logo.tsx
@@ -1,19 +1,22 @@
 "use client";
-import { useFullPath } from "@/lib/use-full-path";
 import Image from "next/image";
-import Link from "next/link";
+import { isProduction } from "@/lib/api/path";
+import { useFullPath } from "@/lib/use-full-path";
+import { Link } from "@/app/navigation";
 
 export function HomeLogo() {
   const pathname = useFullPath();
   const isJuu17Home = pathname === "/";
   const isOneHome = pathname.startsWith("/one");
 
+  const href = isProduction ? "/" : isOneHome ? "/one" : "/brands";
+
   if (isJuu17Home) {
     return <div></div>;
   }
 
   return (
-    <Link href="/one" aria-current="page" className="link-block-2 inline-block">
+    <Link href={href} aria-current="page" className="link-block-2 inline-block">
       <Image
         src={isOneHome ? "/icons/logo-one.svg" : "/icons/logo.svg"}
         loading="lazy"
